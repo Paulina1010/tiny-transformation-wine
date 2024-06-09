0487d84518a1da69f058aa7aa2eb9b772930048e
Paulina1010 <paulina.kucinska10@gmail.com>
2024-06-09 15:30:31 +0000

    Minor changes - modify names of variable

--- tiny-transformation-wine.py
+++ tiny-transformation-wine.py
@@ -15,14 +15,12 @@ kinds = ('White', 'Red', 'Rose', 'Sparkling')
     
 for kind in kinds:
     with open('%s.csv' % kind, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        header = next(spamreader)
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)
         Row = namedtuple('Row', header)
         con.execute("CREATE TABLE IF NOT EXISTS wine(%s, %s, %s, %s, %s, %s, %s, %s, %s)" % (*header, "Kind"))
-        for row in map(convert, map(Row._make, spamreader)):
+        for row in map(convert, map(Row._make, reader)):
             con.execute("INSERT INTO wine VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", (*row, kind))
         cur = con.execute("SELECT * FROM wine")
         for row in cur:
             print(*row)
-
-        
