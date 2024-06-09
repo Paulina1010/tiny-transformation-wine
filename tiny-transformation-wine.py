e439b90572c473b339a40904708bbb6a97b1387f
Paulina1010 <paulina.kucinska10@gmail.com>
2024-06-09 15:26:13 +0000

    Load data for all kinds of wines.

--- tiny-transformation-wine.py
+++ tiny-transformation-wine.py
@@ -10,17 +10,19 @@ def convert(row):
         NumberOfRatings=int(row.NumberOfRatings),
         Price=float(row.Price),
     )
-    
 
-with open('white.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',')
-    header = next(spamreader)
-    Row = namedtuple('Row', header)
-    con.execute("CREATE TABLE IF NOT EXISTS wine(%s, %s, %s, %s, %s, %s, %s, %s, %s)" % (*header, "kind"))
-    for row in map(convert, map(Row._make, spamreader)):
-        con.execute("INSERT INTO wine VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", (*row, "white"))
-    cur = con.execute("SELECT * FROM wine")
-    for row in cur:
-        print(*row)
+kinds = ('White', 'Red', 'Rose', 'Sparkling')
+    
+for kind in kinds:
+    with open('%s.csv' % kind, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        header = next(spamreader)
+        Row = namedtuple('Row', header)
+        con.execute("CREATE TABLE IF NOT EXISTS wine(%s, %s, %s, %s, %s, %s, %s, %s, %s)" % (*header, "Kind"))
+        for row in map(convert, map(Row._make, spamreader)):
+            con.execute("INSERT INTO wine VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", (*row, kind))
+        cur = con.execute("SELECT * FROM wine")
+        for row in cur:
+            print(*row)
 
         
